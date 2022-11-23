93437d29a1166d75a7a7b6aaba22e850283ef52e
Thiago de Paula Monteiro <99137961+thiagomonteiro1info2@users.noreply.github.com>
2022-11-23 09:14:18 -0300

    Add files via upload

---
+++ Livrariacadastro/src/components/template/HeaderComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div class="menu-esquerdo">
+      <span> <RouterLink to="/">Menu</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/categorias">Categorias</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/editoras">Editoras</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/livros/">Cadastrar Livros</RouterLink> </span>
+
+    </div>
+    <div class="menu-direito">
+      <span>Sair</span>
+    </div>
+  </header>
+</template>
+
+<style>
+header span {
+  padding: 0 20px;
+}
+
+header {
+  background-color: #e9f727;
+  background-image:linear-gradient(#ecfc5f,#5f9efc );
+  height: 15%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: rgb(0, 0, 0);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
